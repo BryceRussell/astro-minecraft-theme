4e5c3f17eca616094b8465365f104b64965211fa
First User Name <brycetrussell@gmail.com>
2023-06-20 21:42:56 -0500

    Fix glob pattern

--- packages/astrocraft/tailwind.config.ts
+++ packages/astrocraft/tailwind.config.ts
@@ -23,7 +23,7 @@ function createBackgroundImageClasses(folder: string) {
 }
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/astrocraft/**/*.{astro}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/astrocraft/**/*.astro'],
 	corePlugins: {
     preflight: false,
   },
